0f58dd82c4b356ce3bfe68586292dda7de3e4711
GitHub Action <action@github.com>
2022-03-15 14:44:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/asodja/prototypepsimodifications/MyBundle.kt
+++ src/main/kotlin/com/github/asodja/prototypepsimodifications/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.asodja.prototypepsimodifications
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/asodja/prototypepsimodifications/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.asodja.prototypepsimodifications.services
+
+import com.github.asodja.prototypepsimodifications.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
